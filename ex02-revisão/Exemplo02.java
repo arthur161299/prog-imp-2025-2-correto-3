621078bd4a6a734654aca8522c6cffb20c4fffae
arthur161299 <arthursbsantana16121999@gmail.com>
2025-08-30 00:09:55 +0000

    vetor

--- ex02-revisão/Exemplo02.java
+++ ex02-revisão/Exemplo02.java
@@ -3,7 +3,7 @@ public class Exemplo02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         for(int i=0; i<100; i++){
-            System.out.print(i+" ");
+            System.out.print(i+", ");
         }
         for(int i=1; i<100; i+=2){
             System.out.print(i+", ");
